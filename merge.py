6ef8e7ccdfce7d4dbeddd3c17d45006cd3908df7
kylezehaoqin <qinz@rpi.edu>
2020-07-19 01:10:28 -0700

    Added comments and spec

--- merge.py
+++ merge.py
@@ -3,63 +3,88 @@ import pandas as pd
 import csv
 from decimal import Decimal
 
-path = "/Users/kylechin/Grace-GraceFo_Scrapper/lwe_data/"
-file_type = ".csv"
-all_csv = sorted([file_name for file_name in os.listdir(path) if '.csv' in file_name])
-final_file = "/Users/kylechin/Grace-GraceFo_Scrapper/final.csv"
-# for file in all_csv:
-#   print(file.split("_")[1])
-with open(final_file, "a") as final:
-  writer = csv.writer(final);
-  header = ["Coordinate"]
-  for file in all_csv:
-    with open(path + file, "r") as csv_file:
-      csv_reader = csv.reader(csv_file)
-      counter = 1
-      for row in csv_reader:
-        if counter == 2:
-          date = row[3].split("(")[1].split(")")[0].split()[0]
-          header.append(date)
-          # print(date)
-          break
-        counter += 1
-    csv_file.close()
-    
-  writer.writerow(header)
+def mergeCSV(path, file_type=".csv", final_filename="test.csv"):
+  
+  # sort all csv files under this directory by ascending date order
+  all_csv = sorted([file_name for file_name in os.listdir(path) if file_type in file_name])
+  final_file = os.getcwd() + final_filename + file_type
+  # print(all_csv)
+
+  # write the header bar by collecting time from all files to be merged
+  with open(final_file, "a") as final:
+    writer = csv.writer(final);
+    header = ["Coordinate"]
+    for file in all_csv:
+      with open(path + file, "r") as csv_file:
+        csv_reader = csv.reader(csv_file)
+        counter = 1
+        for row in csv_reader:
+          if counter == 2:
+            date = row[3]
+            header.append(date)
+            # print(date)
+            break
+          counter += 1
+      csv_file.close()
+    writer.writerow(header)
+  final.close()
+
+  # Use dictionary with Coordiante tuples as keys, and a list of lwe data as correspoonding values
   all_data = dict();
-final.close()
 
-with open(path + all_csv[0], "r") as file:
-  csv_reader = csv.reader(file)
-  for row in csv_reader:
-    if row[0] != "Latitude":
-      coord = (float(row[0]), float(row[1]))
-      # print(row[0], row[1], row[2])
-      all_data[coord] = list()
-      all_data[coord].append(Decimal(row[2]))
-      # print(coord, all_data[coord])
-file.close()
-
-for i in range(1, len(all_csv)):
-  with open(path + all_csv[i], "r") as file:
+  # use the first file in directory to initialize the dictionary, it'll eventually be there why not now
+  with open(path + all_csv[0], "r") as file:
     csv_reader = csv.reader(file)
     for row in csv_reader:
       if row[0] != "Latitude":
         coord = (float(row[0]), float(row[1]))
-        all_data[coord].append(Decimal(row[2]))
+        # print(row[0], row[1], row[2])
+        all_data[coord] = list()
+        if (row[2] == "--"):
+          all_data[coord].append(0)
+        else: 
+          all_data[coord].append(Decimal(row[2]))
         # print(coord, all_data[coord])
   file.close()
-# for i in all_data.keys():
-#   print(i, all_data[i])
-with open(final_file, "a") as final:
-  csv_writer = csv.writer(final)
-  counter = 0
-  for i in all_data.keys():
-    row = all_data[i]
-    # print(row)
-    if not all(val == 0 for val in row):
-      row.insert(0, i)  
-      csv_writer.writerow(row)
-    
-final.close()
+
+  # keep doing it
+  for i in range(1, len(all_csv)):
+    with open(path + all_csv[i], "r") as file:
+      csv_reader = csv.reader(file)
+      for row in csv_reader:
+        if row[0] != "Latitude":
+          coord = (float(row[0]), float(row[1]))
+          if (row[2] == "--"):
+            all_data[coord].append(0)
+          else: 
+            all_data[coord].append(Decimal(row[2]))
+          # print(coord, all_data[coord])
+    file.close()
+
+  # for i in all_data.keys():
+  #   print(i, all_data[i])
+
+  # The final csv file containing Coordinate, Time as the header bar, coordinates in tuples under Coordinate, and land water equivalence data below Time.
+  with open(final_file, "a") as final:
+    csv_writer = csv.writer(final)
+    counter = 0
+    for i in all_data.keys():
+      row = all_data[i]
+      # print(row)
+      # If all the elements of current corrdinate are not defined float, ignore them.
+      if not all(val == 0 for val in row):
+        row.insert(0, i)  
+        csv_writer.writerow(row)
+      
+  final.close()
+
+if __name__ == "__main__":
+
+  grace_path = os.getcwd() + "/grace_lwe_data/"
+  gracefo_path = os.getcwd() + "/gracefo_lwe_data/"
+  file_type = ".csv"
+  grace_filename = "/grace_lwe_data"
+  gracefo_filename = "/gracefo_lwe_data"
+  # mergeCSV(grace_path, file_type, grace_filename)
+  mergeCSV(gracefo_path, file_type, gracefo_filename)
       
--- nc_extract.py
+++ nc_extract.py
@@ -2,14 +2,17 @@ import numpy as np
 import netCDF4 as nc
 import pandas as pd
 import csv
+import os
 
-
+# Reads netCDF file from specified path and extract data within the coordinate bounding box
 def readNC(file_path, lat_max, lat_min, lon_max, lon_min):
-	current_dir = "/Users/kylechin/Grace-GraceFo_Scrapper/lwe_data/"
+
+	current_dir = os.getcwd() + "/grace_lwe_data/"
 	file = nc.Dataset(file_path, mode="r")
 	source_name = file_path.split(".")[0].split("/")
 	source_name = source_name[len(source_name)-1]
-	# print(source_name)
+	print(source_name)
+	
 	print("File being processed is " + file_path)
 	for i in file.variables.keys():
 		print(i, end='\t')
@@ -20,8 +23,9 @@ def readNC(file_path, lat_max, lat_min, lon_max, lon_min):
 	lwe = file.variables["lwe_thickness"]
 	time = file.variables["time"]
 	dtime = nc.num2date(time[:], time.units)
+	dtime = str(dtime).split("(")[1].split(")")[0].split()[0]
 	print(file["lwe_thickness"].dimensions)
-	with open(current_dir + source_name + ".csv", "a", newline="") as target:
+	with open(source_name.split("_")[1] + ".csv", "a", newline="") as target:
 		writer = csv.writer(target)
 		writer.writerow(("Latitude", "Longitude", "LWE"))
 		for i in range(len(lat)):
@@ -40,7 +44,8 @@ def readNC(file_path, lat_max, lat_min, lon_max, lon_min):
 if __name__ == "__main__":
     
 	
-	file_path = "/Users/kylechin/Grace-GraceFo_Scrapper/allData/tellus/L3/gracefo/land_mass/RL06/v03/JPL/GRD-3_2018152-2018181_GRFO_JPLEM_BA01_0600_LND_v03.nc"
+	file_path = os.getcwd() + "/allData/tellus/L3/gracefo/land_mass/RL06/v03/JPL/GRD-3_2018152-2018181_GRFO_JPLEM_BA01_0600_LND_v03.nc"
 	# South African latitude and longitude bounding box
 	lat_max, lat_min, lon_max, lon_min = -47, -22, 38.5, 16.5
 	readNC(file_path, lat_max, lat_min, lon_max, lon_min)
+
